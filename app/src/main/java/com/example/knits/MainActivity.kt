174902746486d0ab7ecc14ebb6ea8c0d2ea996a0
Vijay Moorthy <vijaymkpm@gmail.com>
2024-08-15 21:55:42 +0530

    Made changes in the views

--- app/src/main/java/com/example/knits/MainActivity.kt
+++ app/src/main/java/com/example/knits/MainActivity.kt
@@ -4,10 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,21 +15,9 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        val singleJerseyButton = findViewById<Button>(R.id.button_single_jersey)
+        val singleJerseyButton = findViewById<MaterialButton>(R.id.button_single_jersey)
         singleJerseyButton.setOnClickListener(View.OnClickListener {
             val intent = Intent(this.baseContext, SJ_Activity::class.java)
-            startActivity(intent)
-        })
-
-        val ribButton  = findViewById<Button>(R.id.onexone_rib)
-        ribButton.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this.baseContext, RIB_Activity::class.java)
-            startActivity(intent)
-        })
-
-        val interlockButton  = findViewById<Button>(R.id.interlock_button)
-        interlockButton.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this.baseContext, Interlock_Activity::class.java)
             startActivity(intent)
         })
     }
--- app/src/main/java/com/example/knits/SJ_Activity.kt
+++ app/src/main/java/com/example/knits/SJ_Activity.kt
@@ -3,10 +3,15 @@ package com.example.knits
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.radiobutton.MaterialRadioButton
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+
 
 class SJ_Activity : AppCompatActivity() {
     private lateinit var editTextCylinderDiameter: EditText
@@ -18,21 +23,36 @@ class SJ_Activity : AppCompatActivity() {
     private lateinit var buttonClear: Button
     private lateinit var textViewResult: TextView
     private lateinit var buttonCalculate: Button
+    private lateinit var menuAutomcompleteTextview: MaterialAutoCompleteTextView
+    private lateinit var radioGroup : RadioGroup
+    private lateinit var englishRadioButton : MaterialRadioButton
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_singlejersey)
+
+        menuAutomcompleteTextview = findViewById<MaterialAutoCompleteTextView>(R.id.menuAutocompleteTextview)
+        val items = listOf("Single Jersey", "1X1 RIB", "Interlock", "Option 4")
+        val adapter = ArrayAdapter(this.baseContext, R.layout.list_item, items)
+        menuAutomcompleteTextview.setAdapter(adapter)
+        menuAutomcompleteTextview.setText("Single Jersey", false)
+
+
         editTextCylinderDiameter = findViewById<EditText>(R.id.editTextCylinderDiameter)
         editTextMachineGauge = findViewById<EditText>(R.id.editTextMachineGauge)
         editTextFabricGSM = findViewById<EditText>(R.id.editTextFabricGSM)
         editTextFabricWidth = findViewById<EditText>(R.id.editTextFabricWidth)
         editTextYarnCount = findViewById<EditText>(R.id.editTextYarnCount)
+
         editTextStitchLength = findViewById<EditText>(R.id.editTextStitchLength)
 
         buttonClear = findViewById<Button>(R.id.buttonClear)
         textViewResult = findViewById<TextView>(R.id.textViewResult)
-
+        radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        englishRadioButton = findViewById<MaterialRadioButton>(R.id.radio_button_1)
+        englishRadioButton.isChecked = true
 
         // Formula
         //WEPI
@@ -55,9 +75,23 @@ class SJ_Activity : AppCompatActivity() {
             val fabricWidth = editTextFabricWidth.getText().toString().toDouble()
             val yarnCount = editTextYarnCount.getText().toString().toDouble()
             val stitchLength = editTextStitchLength.getText().toString().toDouble()
+            val selectedOption = menuAutomcompleteTextview.getText().toString();
+
 
             val wpi = (3.142 * 2.54 * cylinderDiameter * machineGauge) / fabricWidth
-            val cpi = (fabricGSM * yarnCount * 1.09238) / (wpi * stitchLength)
+
+            var multiplyFactor = 0.546192
+            if(selectedOption.equals("Single Jersey"))
+            {
+                multiplyFactor = 1.09238
+            }
+
+            var yarnCountCalc = yarnCount
+            if(englishRadioButton.isChecked){
+                yarnCountCalc = 5315/yarnCount
+            }
+            val cpi = (fabricGSM * yarnCountCalc * multiplyFactor) / (wpi * stitchLength)
+            println()
 
             textViewResult.setText(String.format("WPI: %.2f\nCPI: %.2f", wpi, cpi))
         } catch (e: NumberFormatException) {
@@ -73,5 +107,7 @@ class SJ_Activity : AppCompatActivity() {
         editTextYarnCount.setText("")
         editTextStitchLength.setText("")
         textViewResult.setText("")
+        menuAutomcompleteTextview.setText("Single Jersey", false)
+        englishRadioButton.isChecked = true
     }
 }
